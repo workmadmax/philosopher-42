83b225606d5a729c3b8f9c64fc2a4a8b3e2664a1
workmadmax <work.madmax@gmail.com>
2023-04-30 19:13:56 -0300

    checkers for arguments

---
+++ srcs/checkers/arguments.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   arguments.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: madmax42 <madmax42@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/30 17:30:27 by madmax42          #+#    #+#             */
+/*   Updated: 2023/04/30 19:12:53 by madmax42         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	check_args_isdigit(char *args)
+{
+	int	idx;
+
+	idx = 0;
+	if (args[idx] == '+' || args[idx] == '-')
+		idx++;
+	if (!args[idx])
+		return (1);
+	while (args[idx])
+	{
+		if (!ft_isdigit(args[idx]))
+			return (1);
+		idx++;
+	}
+	return (0);
+}
+
+int	check_int_overflow(char *args)
+{
+	int	size;
+
+	size = 0;
+	while (args[size])
+		size++;
+	if (size > 11)
+		return (1);
+	if (size == 11 && ft_atol(args) > INT_MAX)
+		return (1);
+	if (size == 10 && ft_atol(args) > INT_MAX)
+		return (1);
+	return (0);
+}
+
+int	check_positive_number(char *args)
+{
+	if (ft_atol(args) <= 0)
+		return (1);
+	return (0);
+}
+
+int	check_input_validity(int argc, char **argv)
+{
+	int	idx;
+
+	idx = 1;
+	if (argc < 5 || argc > 6)
+		return (1);
+	while (argv[idx])
+	{
+		if (check_args_isdigit(argv[idx]))
+			return (1);
+		if (check_int_overflow(argv[idx]))
+			return (1);
+		if (check_positive_number(argv[idx]))
+			return (1);
+		idx++;
+	}
+	return (0);
+}
